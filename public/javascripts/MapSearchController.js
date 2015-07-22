9c736708aae448f872eb9e139a1c5bced252b7e6
oleg <oleg.gasul@gmail.com>
2015-07-22 15:24:32 +0300

    Added nearest by car route

--- public/javascripts/MapSearchController.js
+++ public/javascripts/MapSearchController.js
@@ -25,7 +25,7 @@ function MapSearchController(defaultCity) {
 
     this.search = function(text, callback) {
         $.ajax({
-            url: "https://geocode-maps.yandex.ru/1.x/?geocode=" + encodeURIComponent(text) + "&lang=ru-RU&format=json",
+            url: document.location.protocol + "//geocode-maps.yandex.ru/1.x/?geocode=" + encodeURIComponent(text) + "&lang=ru-RU&format=json",
             type: "get",
             dataType: "json"
         }).done(function(data) {
@@ -38,7 +38,7 @@ function MapSearchController(defaultCity) {
 
     this.calculateRoute = function(from, to, callback) {
         $.ajax({
-            url: "https://api.visicom.ua/data-api/2.0/core/distance.json?origin=" + from + "&destination=" + to + "&geometry=path&key=" + VISICOM_AUTH_KEY,
+            url: document.location.protocol + "//api.visicom.ua/data-api/2.0/core/distance.json?origin=" + from + "&destination=" + to + "&geometry=path&key=" + VISICOM_AUTH_KEY,
             type: "get",
             dataType: "json"
         }).done(function(data) {
@@ -48,6 +48,27 @@ function MapSearchController(defaultCity) {
         });
     }
 
+    function calculateDistanceMatrix(from, streets, maxDistance, callback) {
+        var destinations = [];
+        for (var i = 0; i < streets.length; i++) {
+            destinations.push(streets[i].id);
+        }
+
+        $.ajax({
+            url: document.location.protocol + "//api.visicom.ua/data-api/2.0/core/distancematrix.json?origins=" + from + "&destinations=" + destinations.join('|') + "&mode=driving-shortest&key=" + VISICOM_AUTH_KEY,
+            type: "get",
+            dataType: "json"
+        }).done(function(response) {
+            for (var i = 0; i < response.rows[0].length; i++) {
+                var distance = response.rows[0][i].distance;
+                if (distance < maxDistance)
+                    streets[i].distance = distance;
+            }
+            callback(streets);
+        }).error(function() {
+            console.dir(arguments);
+        });
+    }
 
     function convertNearestResponse(response) {
         var result = [];
@@ -64,11 +85,23 @@ function MapSearchController(defaultCity) {
 
     this.findNearests = function(from, radius, callback) {
         $.ajax({
-            url: "https://api.visicom.ua/data-api/2.0/ru/search/adr_street.json?near=" + from.lng + "," + from.lat + "&radius=" + radius + "&key=" + VISICOM_AUTH_KEY,
+            url: document.location.protocol + "//api.visicom.ua/data-api/2.0/ru/search/adr_street.json?near=" + from.lng + "," + from.lat + "&radius=" + radius + "&key=" + VISICOM_AUTH_KEY,
             type: "get",
             dataType: "json"
         }).done(function(response) {
-            callback(convertNearestResponse(response));
+            var streets = [];
+            for (var i = 0; i < response.features.length; i++) {
+                var feature = response.features[i];
+                if (feature.properties.settlement_id != "STL1NQ7EP")
+                    continue;
+                streets.push({id: feature.id, name: feature.properties.name, type: feature.properties.type});
+            }
+
+            calculateDistanceMatrix(from.lng + "," + from.lat, streets, 15000, function(data) {
+                callback(data);
+            });
+
+            //callback(convertNearestResponse(response));
         }).error(function() {
             console.dir(arguments);
         });
