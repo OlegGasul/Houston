736c5a1b08355c88b68daea3584a7b7cbf8abafb
oleg <oleg.gasul@gmail.com>
2015-09-03 11:02:55 +0300

    .

--- public/javascripts/Helpers.js
+++ public/javascripts/Helpers.js
@@ -1,11 +1,13 @@
 function Helpers() {
 
     this.toFixed = function(array, digits) {
+        var result = [];
+        
         for (var i = 0; i < array.length; i++) {
-            array[i] = array[i].toFixed(digits)
+            toFixed.push(array[i].toFixed(digits));
         }
 
-        return array;
+        return result;
     }
 
 }
