8c3040eacb15d8818a3691c70018ca153b7b57e9
oleg <oleg.gasul@gmail.com>
2015-09-05 12:25:18 +0300

    Fixed Helpers

--- public/javascripts/Helpers.js
+++ public/javascripts/Helpers.js
@@ -1,6 +1,9 @@
 function Helpers() {
 
     this.toFixed = function(array, digits) {
+        if (array.lat && array.lng)
+            return this.toFixed([array.lat, array.lng]);
+
         var result = [];
 
         for (var i = 0; i < array.length; i++) {
